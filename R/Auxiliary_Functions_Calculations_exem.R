92c6577fb7e8f3de689c1acd51a954e7ea3b05b5
Dorleta <dgarcia@azti.es>
2015-05-19 13:52:47 +0200

    Another bug in overfishing
    adapted to work with 'any' HCR

--- R/Auxiliary_Functions_Calculations_exem.R
+++ R/Auxiliary_Functions_Calculations_exem.R
@@ -10,7 +10,11 @@ overfishing <- function(biols, fleets, advice.ctrl, year){
   if(is.numeric(year)) year <- dimnames(biols[[1]]@n)[[2]][year]
   obj <- list(biols = biols, fleets = fleets)
   f    <- F_flbeia(obj, years = year)
-  fmsy <- lapply(advice.ctrl, function(x) ifelse(is.null(x$ref.pts['Fmsy',]), NA, x$ref.pts['Fmsy',]))
+  
+  Fref <- dimnames(x$ref.pts)[[1]]
+  Fref <- Fref[which(substr(Fref, 1,1) == 'F')]
+  
+  fmsy <- lapply(advice.ctrl, function(x) ifelse(is.null(x$ref.pts[Fref,]), NA, x$ref.pts[Fref,]))
   res <- sapply(names(fmsy), function(x) fmsy[[x]] < f[x,,])
   res <- ifelse(is.na(res), FALSE, res)
   if(!is.null(dim(res))) res <- t(res) # if there are iterations put them in columns not in rows.
