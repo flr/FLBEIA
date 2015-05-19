1bf6cd4a07f94b1aa926a1a5ab6e797f7b557fc8
Dorleta <dgarcia@azti.es>
2015-05-19 14:33:38 +0200

    Another bug in overfishing

--- R/Auxiliary_Functions_Calculations_exem.R
+++ R/Auxiliary_Functions_Calculations_exem.R
@@ -6,16 +6,29 @@
 #-------------------------------------------------------------------------------------------
 # overfishing: Are the stock suffering of overfishing, returns a matrix [nst,nit]
 #-------------------------------------------------------------------------------------------
-overfishing <- function(biols, fleets, advice.ctrl, year){
+overfishing_f <- function(biols, fleets, advice.ctrl, year){
   if(is.numeric(year)) year <- dimnames(biols[[1]]@n)[[2]][year]
   obj <- list(biols = biols, fleets = fleets)
   f    <- F_flbeia(obj, years = year)
   
-  Fref <- dimnames(x$ref.pts)[[1]]
-  Fref <- Fref[which(substr(Fref, 1,1) == 'F')]
+
   
-  fmsy <- lapply(advice.ctrl, function(x) ifelse(is.null(x$ref.pts[Fref,]), NA, x$ref.pts[Fref,]))
-  res <- sapply(names(fmsy), function(x) fmsy[[x]] < f[x,,])
+  fmsy <- numeric(length(biols))
+  names(fmsy) <- names(biols)
+  
+  for(st in names(biols)){
+    Fref <- dimnames(advice.ctrl[[st]]$ref.pts)[[1]]
+    Fref <- Fref[which(substr(Fref, 1,1) == 'F')] 
+    
+    if(length(dim(advice.ctrl[[st]]$ref.pts)) == 3) Fref <-  advice.ctrl[[st]]$ref.pts[Fref,year,]
+    if(length(dim(advice.ctrl[[st]]$ref.pts)) == 2) Fref <- advice.ctrl[[st]]$ref.pts[Fref,year]
+    if(length(dim(advice.ctrl[[st]]$ref.pts)) == 1) Fref <- advice.ctrl[[st]]$ref.pts[Fref]
+      
+    fmsy[st] <- ifelse(is.null(Fref), NA, Fref)
+  }
+
+  
+  res <- sapply(names(fmsy), function(x) fmsy[x] < f[x,,])
   res <- ifelse(is.na(res), FALSE, res)
   if(!is.null(dim(res))) res <- t(res) # if there are iterations put them in columns not in rows.
   
