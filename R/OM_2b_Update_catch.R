24652aa0a90bf8b0abc57dd42a287fe1bff77637
Itsaso <icarmona@azti.es>
2015-11-02 16:55:47 +0100

    Update OM_2b_Update_catch.R

--- R/OM_2b_Update_catch.R
+++ R/OM_2b_Update_catch.R
@@ -72,8 +72,8 @@ CobbDouglasBio.CAA  <- function(fleets, biols, fleets.ctrl, advice, year = 1, se
     
     if(!(st %in% sts)) return(fleets)
     
-    # catch restriction, if empty => landings.
-    catch.restr <- ifelse(is.null(fleets.ctrl[[flnm]]$restriction), 'landings', fleets.ctrl[[flnm]]$restriction)
+    # catch restriction, if empty => landings or catch.
+    catch.restr <- ifelse(is.null(fleets.ctrl[[flnm]]$restriction), 'landings',ifelse(length(fleets.ctrl[[flnm]]$restriction)==1, fleets.ctrl[[flnm]]$restriction,fleets.ctrl[[flnm]]$restriction[yr]))
                                              
     tac <- rep('Inf',it)
     
@@ -207,7 +207,8 @@ CobbDouglasAge.CAA <- function(fleets, biols, fleets.ctrl, advice, year = 1, sea
     tac <- rep('Inf',it)
  
     # catch restriction, if empty => landings.
-    catch.restr <- ifelse(is.null(fleets.ctrl[[flnm]]$restriction), 'landings', fleets.ctrl[[flnm]]$restriction)
+    catch.restr <- ifelse(is.null(fleets.ctrl[[flnm]]$restriction), 'landings',ifelse(length(fleets.ctrl[[flnm]]$restriction)==1, fleets.ctrl[[flnm]]$restriction,fleets.ctrl[[flnm]]$restriction[yr]))
+
     
  
     #  quota share % to be upodate du to year transfer.
