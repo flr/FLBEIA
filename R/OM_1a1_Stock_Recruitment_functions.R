66ce3ef3683c80a1db1ac7f55a0614cd29923a75
Sonia Sánchez <ssanchez@azti.es>
2019-03-29 10:57:47 +0100

    New SR function added: segregmix
    Mixture of two segmented regression models and a covariate for estimating uncertainty above Blim given uncertainty in low regimes and sd of the residuals

--- R/OM_1a1_Stock_Recruitment_functions.R
+++ R/OM_1a1_Stock_Recruitment_functions.R
@@ -106,3 +106,33 @@ ricker.low <- function(){
   return(list(logl = logl, model = model, initial = initial))
 }
 
+
+# rec is calculated as:
+# - a * SSB       , if     SSB <= b
+# - a * b         , if b < SSB <= B
+# - A * B * uncAdd, if B < SSB
+# 
+# where uncAdd corresponds to the uncertainty for SSB > B,
+# with uncAdd = @uncertainty ^ ((sd of SR residuals for SSB <= B) / (sd of SR residuals for SSB <= B) - 1)
+
+segregmix <- function () 
+{
+  logl <- function(a, b, A, B, rec, ssb, uncAdd) {
+    loglAR1(log(rec), FLQuant(log(ifelse( c(ssb) <= b, a * c(ssb), 
+                                          ifelse( c(ssb) <= B, a * b, 
+                                                  A * B * uncAdd))), dimnames = dimnames(ssb)))
+  }
+  
+  model <- rec ~ FLQuant(ifelse( c(ssb) <= b, a * c(ssb), 
+                                 ifelse( c(ssb) <= B, a * b, 
+                                         A * B * uncAdd)), dimnames = dimnames(ssb))
+  
+  initial <- structure(function(rec, ssb) {
+    return(FLPar(a = median(c(rec)/c(ssb), na.rm = TRUE), 
+                 b = median(c(ssb), na.rm = TRUE), 
+                 A = median(c(rec)/c(ssb), na.rm = TRUE), 
+                 B = median(c(ssb), na.rm = TRUE)))
+  }, lower = rep(0, 0, 0, 0), upper = rep(Inf, 4))
+  
+  return(list(logl = logl, model = model, initial = initial))
+}
