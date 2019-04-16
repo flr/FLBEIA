904020c820e5c1e9bdbd8cc6d36421ac11c8e527
Sonia Sánchez <ssanchez@azti.es>
2019-04-16 10:07:49 +0200

    FroeseHCR: some corrections

--- R/MP_3c_HCR_Froese.R
+++ R/MP_3c_HCR_Froese.R
@@ -1,23 +1,27 @@
 #-------------------------------------------------------------------------------
-# HCR proposed in Froese et. al 2010 - Fish & Fisheries
+# HCR proposed in Froese et. al 2011 - Fish & Fisheries
 # Biomass Based HCR.
 #  Reference Points: Btrigger, Blim and Btarget.
 #   The proposal in the paper is:
-#           - Btrigger = alpha_0 * Bmsy
-#           - Btarget  = alpha_1 * Bmsy
-#           - Blim     = alpha_2 * Bmsy
+#           - Blim     = alpha_0 * Bmsy
+#           - Btrigger = alpha_1 * Bmsy
+#           - Btarget  = alpha_2 * Bmsy (not required for the rule)
 #
-#  - alpha_0 = 1, alpha_1 = 1.3 and alpha_2 = 0.5 are optional in this
+#  - alpha_0 = 0.5, alpha_1 = 1 are optional in this
 #       implementation of the HCR.
+#
 #  - TAC advice depending on B in relation to BRP is:
-#           - 0.91*MSY         , if B > Btarget
-#           - 0.91*f[beta]*MSY , if Btrigger < B < Btarget
-#           - 0.0               , if B < Btrigger
+#           - 0.0                 , if B < Btrigger
+#           - beta * MSY * f[Bmsy], if Btrigger <= B < Btarget
+#           - MSY                 , if B >= Btarget
 #   - beta = 0.91 is optional in this implementation of the HCR.
+#   - f[Bmsy] = (B/Bmsy - alpha_0) / (alpha_1 - alpha_0)
 #
 # 06/09/2011 12:11:51
+# Changed: 05/03/2019 - Sonia Sanchez (some corrections)
 #-------------------------------------------------------------------------------
 #' @rdname annualTAC
+#' @aliases FroeseHCR
 
 FroeseHCR <- function(stocks, advice, advice.ctrl, year, stknm,...){
 
@@ -25,7 +29,7 @@ FroeseHCR <- function(stocks, advice, advice.ctrl, year, stknm,...){
     ageStruct <- ifelse(dim(stk@m)[1] > 1, TRUE, FALSE)
 
     ref.pts  <- advice.ctrl[[stknm]]$ref.pts # matrix[5,it]  rows = Bmsy, MSY, alpha_0, alpha_1, beta
-
+    
     iter     <- dim(stk@m)[6]
     yrsnames <- dimnames(stk@m)[[2]]
     yrsnumbs <- as.numeric(yrsnames)
@@ -46,8 +50,10 @@ FroeseHCR <- function(stocks, advice, advice.ctrl, year, stknm,...){
     b.pos <- apply(matrix(1:iter,1,iter),2, function(i) findInterval(b.datyr[i], Brefs[,i]))  # [it]
 
     # Calculate the TAC.
-    mult <- 1/(1-ref.pts['alpha_0',])*(-ref.pts['alpha_0',] + b.datyr/Brefs[2,])
-    TAC  <- ifelse(b.pos == 0, 0, ifelse(b.pos == 1, ref.pts['MSY',]*ref.pts['beta',]*mult,ref.pts['MSY',]*ref.pts['beta',]))
+    mult <- 1/(ref.pts['alpha_1',] - ref.pts['alpha_0',]) * 
+      (b.datyr/ref.pts['Bmsy',] - ref.pts['alpha_0',])
+    TAC  <- ifelse(b.pos == 0, 0, ifelse(b.pos == 1, ref.pts['MSY',]*ref.pts['beta',]*mult,
+                                         ref.pts['MSY',]*ref.pts['beta',]))
     
     advice[['TAC']][stknm,year+1,,,,] <- TAC
 
