d0ebf39dd5ea1487c60c37cb849ac756a67d099f
Paul Dolder <paul.dolder@gmail.com>
2020-05-11 11:15:43 +0100

    fix bug

--- R/OM_2a_Effort_Dynamics_SMFB_VarEffortShare.R
+++ R/OM_2a_Effort_Dynamics_SMFB_VarEffortShare.R
@@ -486,7 +486,7 @@ make_RUM_predict_df <- function(model = NULL, fleet = NULL, season) {
   ## Construct the dataframe
   predict.df <- expand.grid(metier = fleet@metiers@names, 
                             choice = "yes", 
-                            season = seas), 
+                            season = seas, 
                             vcost = v, 
                             effshare = e,
                             stringsAsFactors = FALSE)
