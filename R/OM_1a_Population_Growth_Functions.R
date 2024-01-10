7a4a5edfb24ada8155269c6b82ad451104ca013a
Dorleta <dgarcia@azti.es>
2024-01-10 09:05:07 +0100

    Problem with ASPG_DDW solved

--- R/OM_1a_Population_Growth_Functions.R
+++ R/OM_1a_Population_Growth_Functions.R
@@ -349,5 +349,30 @@ ASPG_Baranov <- function(biols, SRs, fleets, year, season, stknm, ...){
 } 
 
     
-
+# Age structured population growth with densodependence
+#### ASPG_DDW ----
+ASPG_DDW <- function(biols, SRs, fleets, year, season, stknm, biols.ctrl,...){
+  
+  # update wt of stknm
+  ddw.model <- biols.ctrl[[stkm]][['DDW.model']]
+  lfd       <- biols.ctrl[[stkm]][['LFD']]
+  a         <- biols.ctrl[[stkm]][['a.lw']]
+  lbins     <- as.numeric(colNames(lfd))
+  
+  B <- quantSums((biols[[stkm]]@wt*biols[[stkm]]@n)[,year-1])[drop=T]
+  
+  condF <- predict(LW_lm, data.frame(biomass = B))
+  
+  wy <- a*(lbins)^condF
+  
+  wt <- rowSums(sweep(lfd, 2, wy, "*"))
+  
+  biols[[stkm]]@wt[,year] <- wt
+  
+  # use normal ASPG to project the population
+  
+  res <- ASPG(biols, SRs, fleets, year, season, stknm, biols.ctrl,...)
+  
+  return(res)
+}
 
